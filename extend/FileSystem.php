27c7a9c178530b625d764f7d2323bbad1f6089cd
tushan <51120332+tsxcw@users.noreply.github.com>
2022-07-10 15:56:49 +0800

    Update FileSystem.php

--- extend/FileSystem.php
+++ extend/FileSystem.php
@@ -16,11 +16,6 @@ class FileSystem{
      */
   public static function delFile($path){
       unlink(env("fileSystem.root").'/'.$path);//删除本地数据
-      try {
-        file_get_contents('https://www.imgurl.org/api/dcache.php?url='.env('app.cdn').'/'.$path);
-      } catch (\Throwable $th) {
-        //throw $th;
-      }
       CosTencent::unlink($path);//删除云端数据
   }
 }

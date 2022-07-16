c8e2c93d3d11972ad361b8caeb0198899020042b
涂山 <1756328925@qq.com>
2022-07-16 13:06:54 +0800

    修改env配置文件

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             // 是否需要断线重连
             'break_reconnect' => true,
             // 监听SQL
-            'trigger_sql'     => env('app_debug', false),
+            'trigger_sql'     => env('database.debug', false),
             // 开启字段缓存
             'fields_cache'    => false,
         ],
